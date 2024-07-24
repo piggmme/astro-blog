e151c07c1cfa7d8ed6a076263807e2dcca941b6c
cherry.a <cherry.a@kakaocorp.com>
2024-07-24 19:46:03 +0900

    fix: 타입 오류 수정

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,6 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 
-export async function GET (context) {
+export async function GET (context: { site: string }) {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
